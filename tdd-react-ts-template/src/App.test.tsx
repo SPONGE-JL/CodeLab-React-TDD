c020b445bd9a425e98a795893752cda60f29f319
SPONGE-JL <dev2sponge@gmail.com>
2021-10-14 00:16:11 +0900

    tdd-react-ts-tpl: add jest style test case

--- tdd-react-ts-template/src/App.test.tsx
+++ tdd-react-ts-template/src/App.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<App />);
+
+    // Check Link
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+    // Check Image Tag (App-logo)
+    const elemByClass_AppLogo = container.getElementsByClassName('App-logo');
+    expect(elemByClass_AppLogo).toHaveLength(1);
+    expect(elemByClass_AppLogo[0]).toHaveAttribute('src', 'logo.svg');
+
+    // Check P Tag
+    const elmByP = container.getElementsByTagName('p');
+    expect(elmByP).toHaveLength(1);
+    expect(elmByP[0]).toHaveTextContent('Edit src/App.js and save to reload');
+
+    // Snapshot
+    expect(container).toMatchSnapshot();
+  });
 });
