76774bd251fe0d08725d1d88137d59f2b812622d
sponge <dev2sponge@gmail.com>
2021-10-27 19:07:44 +0900

    todo-list: refact app.tsx with test

--- hands-on-todo-list/src/App.test.tsx
+++ hands-on-todo-list/src/App.test.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('correctly renders components', () => {
+    const { container } = render(<App />);
+
+    const AppLink = screen.getByText(/learn react/i);
+    expect(AppLink).toBeInTheDocument();
+
+    const AppLogo = screen.getByAltText('AppLogo');
+    expect(AppLogo).toBeInTheDocument();
+    expect(AppLogo).toHaveAttribute('src', 'logo.svg');
+
+    const pElements = container.getElementsByTagName('p');
+    expect(pElements).toHaveLength(1);
+    expect(pElements[0]).toHaveTextContent('Edit src/App.tsx and save to reload.');
+
+    expect(container).toMatchSnapshot();
+  });
 });
