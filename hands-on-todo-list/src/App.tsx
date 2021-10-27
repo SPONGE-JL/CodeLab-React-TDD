898b73d255f160d8be7317062c27c369c137aa31
sponge <dev2sponge@gmail.com>
2021-10-28 02:04:54 +0900

    todo-list: InputBox & Button

--- hands-on-todo-list/src/App.tsx
+++ hands-on-todo-list/src/App.tsx
@@ -1,56 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import Styled from 'styled-components';
 
-const Container = Styled.div`
-  text-align: center;
-`;
+import { Button, Input } from 'Components';
 
-const Header = Styled.header`
-  background-color: #282c34;
+//- Sytled Component
+const Container = Styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  text-align: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `;
 
-const AppLogo = Styled.img`
-  height: 40vmin;
-  pointer-events: none;
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: App-logo-spin infinite 20s linear;
-  }
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
+const Contents = Styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border-radius: 8px;
+  background-color: : #FFFFFF;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
-const AppLink = Styled.a`
-color: #61dafb;
+const InputContainer = Styled.div`
+  display: flex;
+  width: 50%;
+  min-width: 400px;
 `;
 
+//- React Component
 function App() {
   return (
     <Container>
-      <Header>
-        <AppLogo alt="AppLogo" src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </AppLink>
-      </Header>
+      <Contents>
+        <InputContainer>
+          <Input placeholder="Insert a new task" onChange={(inputText) => console.log(inputText)} />
+          <Button label="Add" onClick={() => alert('Click Event! (Add)')} />
+        </InputContainer>
+      </Contents>
     </Container>
   );
 }
---
+++ hands-on-todo-list/src/Components/Input/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+//- Sytled Component
+const InputBox = Styled.input`
+  font-size: 16px;
+  text-align: center;
+  width: 100%;
+  border-radius: 8px;
+  border: 1px solid #BDBDBD;
+  outline: none;
+  padding: 10px;
+  margin-right: 10px;
+`;
+
+//- React Component
+interface InputProps {
+  readonly placeholder?: string;
+  readonly onChange?: (text: string) => void;
+}
+
+export const Input = ({ placeholder, onChange }: InputProps) => {
+  return (
+    <InputBox
+      placeholder={placeholder}
+      onChange={(event) => {
+        if (typeof onChange === 'function') {
+          onChange(event.target.value);
+        }
+      }}
+    />
+  );
+};
